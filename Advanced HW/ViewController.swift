f7e5ef559c7d8091a4f143948db89648157f6f91
Roman Bespalov <voordex@gmail.com>
2022-12-04 14:44:08 +0300

    Initial Commit

---
+++ Advanced HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Advanced HW
+//
+//  Created by Роман Беспалов on 04.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
